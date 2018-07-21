644e5c56b1d0161d37b9912145b49e65eb5543cf
vikrep <protsenko.viktor@gmail.com>
2018-07-21 15:53:46 +0100

    add sort

--- src/components/TableElement.js
+++ src/components/TableElement.js
@@ -4,20 +4,43 @@ import fakeAlbums from '../data/fakeAlbums.json'
 import './TableElement.css'
 
 const headerRow = ['Cover', 'Artist', 'Title', 'Year', 'Rating', 'Catalog_Id']
+const newHead = props => (
+	<th>
+		<td>Cover</td>
+		<td>Cover</td>
+		<td>Cover</td>
+		<td>Cover</td>
+		<td>Cover</td>
+		<td>Cover</td>
+		</th>
+)
+
+const handler = e => console.log('click');
+const altHead = [
+	{ content: 'hello', onClick: handler },
+	{ content: 'hello' },
+	{ content: 'hello' },
+	{ content: 'hello' },
+	{ content: 'hello' },
+	{ content: 'hello' }
+]
 
 const renderBodyRow = ({ cover, artist, title, year, rating, id }, i) => ({
+	key: `result-row-${i}`,
 	cells: [
 		<td><Image src={cover} size='tiny' verticalAlign='middle' bordered /></td>,
-		artist ? { key: 'artist', content: artist } : 'Artist',
-		title ? { key: 'title', content: title  } : 'Title',
-		year ? { key: 'year', content: year,  width: '1' } : 'Year',
+		{ content: artist },
+		{ content: title },
+		{ content: year, width: '1' },
 		<td><Rating icon='star' defaultRating={rating} maxRating={5} size='small' /></td>,
-		id ? { key: 'id', content: id, width: '1' } : 'Catalog_id'
-	],})
+		{ content: id, width: '1' }
+	],
+})
 
 
 const SearchTable = () => (
-	<Table singleLine verticalAlign='middle' textAlign='center' headerRow={headerRow} renderBodyRow={renderBodyRow} tableData={fakeAlbums} />
+	<Table singleLine verticalAlign='middle' textAlign='center'
+	 headerRow={altHead} renderBodyRow={renderBodyRow} tableData={fakeAlbums} />
 )
 
 
