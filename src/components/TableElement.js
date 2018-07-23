e35566b567533db3fc99244abaeb0279c3319afb
mahsa2017 <upmahjour@gmail.com>
2018-07-23 15:20:55 +0100

    error for sort

--- src/components/TableElement.js
+++ src/components/TableElement.js
@@ -1,47 +1,60 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Header, Table, Rating, Image } from 'semantic-ui-react'
 import fakeAlbums from '../data/fakeAlbums.json'
 import './TableElement.css'
+import _ from 'lodash'
 
-const headerRow = ['Cover', 'Artist', 'Title', 'Year', 'Rating', 'Catalog_Id']
-const newHead = props => (
-	<th>
-		<td>Cover</td>
-		<td>Cover</td>
-		<td>Cover</td>
-		<td>Cover</td>
-		<td>Cover</td>
-		<td>Cover</td>
-		</th>
-)
+export default class SearchPage extends Component {
+	render() {
+		const renderBodyRow = ({ cover, artist, title, year, rating, id }, i) => ({
+			key: `result-row-${i}`,
+			cells: [
+				<td><Image src={cover} size='tiny' verticalAlign='middle' bordered /></td>,
+				{ content: artist, width: '1' },
+				{ content: title },
+				{ content: year, width: '1' },
+				<td><Rating icon='star' defaultRating={rating} maxRating={5} size='small' disabled='true' /></td>,
+				{ content: id, width: '1' }
+			],
+		})
+		this.state = {
+			column: null,
+			data: fakeAlbums,
+			direction: null,
+		}
+		const { column, data, direction } = this.state;
+		this.handleSort = clickedColumn => () => {
+			const { column, data, direction } = this.state
+		
+			if (column !== clickedColumn) {
+			  this.setState({
+				column: clickedColumn,
+				data: _.sortBy(data, [clickedColumn]),
+				direction: 'ascending',
+			  })
+		
+			  return
+			}
+		}
+		const altHead = [
+			{ content: 'Cover' },
+			{ content: 'Artist', sorted: column === 'artist' ? direction : null, onClick: this.handleSort('artist') },
+			{ content: 'Title', sorted: column === 'title' ? direction : null, onClick: this.handleSort('title') },
+			{ content: 'Year', sorted: column === 'year' ? direction : null, onClick: this.handleSort('year') },
+			{ content: 'Rating' },
+			{ content: 'catalog #' }
+		]
+		this.setState({
+			data: data.reverse(),
+			direction: direction === 'ascending' ? 'descending' : 'ascending',
+		  })
 
-const handler = e => console.log('click');
-const altHead = [
-	{ content: 'hello', onClick: handler },
-	{ content: 'hello' },
-	{ content: 'hello' },
-	{ content: 'hello' },
-	{ content: 'hello' },
-	{ content: 'hello' }
-]
-
-const renderBodyRow = ({ cover, artist, title, year, rating, id }, i) => ({
-	key: `result-row-${i}`,
-	cells: [
-		<td><Image src={cover} size='tiny' verticalAlign='middle' bordered /></td>,
-		{ content: artist },
-		{ content: title },
-		{ content: year, width: '1' },
-		<td><Rating icon='star' defaultRating={rating} maxRating={5} size='small' /></td>,
-		{ content: id, width: '1' }
-	],
-})
-
-
-const SearchTable = () => (
-	<Table singleLine verticalAlign='middle' textAlign='center'
-	 headerRow={altHead} renderBodyRow={renderBodyRow} tableData={fakeAlbums} />
-)
-
-
-export default SearchTable
+		return (
+			<Table singleLine
+				verticalAlign='middle' textAlign='center'
+				headerRow={altHead}
+				renderBodyRow={renderBodyRow}
+				tableData={fakeAlbums} sortable/>
+		)
+	}
+}
--- src/components/TestPage.js
+++ src/components/TestPage.js
@@ -29,6 +29,18 @@ export default class TestPage extends Component {
                 </Jumbotron>
             </div>
 
+// const headerRow = ['Cover', 'Artist', 'Title', 'Year', 'Rating', 'Catalog_Id']
+// const newHead = props => (
+// 	<th>
+// 		<td>Cover</td>
+// 		<td>Cover</td>
+// 		<td>Cover</td>
+// 		<td>Cover</td>
+// 		<td>Cover</td>
+// 		<td>Cover</td>
+// 		</th>
+// )
+// const handler = e => console.log('click');
 
         )
 
