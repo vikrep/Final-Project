bab9058ecc4b5d96f8d918974ae3dac021ac0139
vikrep <protsenko.viktor@gmail.com>
2018-08-13 22:42:14 +0100

    clear login page

--- src/Admin.js
+++ src/Admin.js
@@ -1,135 +1,117 @@
 
-    import React, {Component} from 'react';
-    import firebase from "firebase"
-    import SearchPage from './components/SearchPage';
-    import {BrowserRouter as Router, Route } from 'react-router-dom';
-    import Welcome from './components/WelcomePage';
-    import Admin from './Admin';
+import React, { Component } from 'react';
+import firebase from "firebase"
 
-    let config = {
-            apiKey: "AIzaSyAjBd4hamvW4AsYwymdJOE5Tiw2BHlLZLQ",
-        authDomain: "collection-69f45.firebaseapp.com",
-        databaseURL: "https://collection-69f45.firebaseio.com",
-        projectId: "collection-69f45",
-        storageBucket: "collection-69f45.appspot.com",
-        messagingSenderId: "775971698132"
-      };
-    
-      firebase.initializeApp(config);
 
-      class Authen extends Component {
-          state = {
-              email: '',
-              password: '',
-              err: '',
-              isLoggedIn: false,
-          };
-      
-          login = () => {
-              const email = this.state.email;
-              const password = this.state.password;
-      
-              const auth = firebase.auth();
-      
-              const promise = auth.signInWithEmailAndPassword(email, password);
-      
-              promise.then(user => {
-                  let message = 'Welcome, you are now logged in!';
-                  this.setState({err: message, isLoggedIn: true});
-                  firebase.auth().onAuthStateChanged(user => {
-                    if(user) {
-                      window.location = 'search'; //After successful login, user will be redirected to search
-                    }
-                  });
-                  
-              });
-              
-      
-              promise.catch(e => {
-                  let err = e.message;
-                  this.setState({err});
-              })
-          };
-      
-          signup = () => {
-              const email = this.state.email;
-              const password = this.state.password;
-      
-              const auth = firebase.auth();
-      
-              const promise = auth.createUserWithEmailAndPassword(email, password);
-      
-              promise
-                  .then(user => {
-                      let err = `Welcome ${user.email}`;
-                      firebase.database().ref(`users/${user.uid}`).set({
-                          email: user.email,
-                      });
-                      this.setState({err});
-                  })
-                  .catch(e => {
-                      let err = e.message;
-                      this.setState({err});
-                  });
-          };
-      
-          logout = () => {
-              let promise = firebase.auth().signOut();
-              promise.then(() => {
-                  let message = 'You are now logged out!';
-                  this.setState({err: message, isLoggedIn: false});
-              }).catch(err => {
-                  this.setState({err: err.message});
-              });
-          };
-      
-          
-          render(){
-              return(
-                  <div>
-                      <input
-                          id="email"
-                          onChange={(e) => {this.setState({email: e.target.value})}}
-                          type="email"
-                          placeholder="Enter your email"
-                      /> <br/>
-                      <input
-                          id="pass"
-                          onChange={(e) => {this.setState({password: e.target.value})}}
-                          type="password"
-                          placeholder="Enter your password"
-                      /> <br/>
-                      <p>{this.state.err}</p>
-                      <button onClick={this.login}>Login</button>
-                      <button
-                          id="logout"
-                          onClick={this.logout}
-                          className={this.state.isLoggedIn ? '' : 'hide'}>Logout</button>
-                      <br/>
-                                    <img alt="3" src="/assets/3.jpg" responsive="true" />
-                  </div>
-              );
-          }
-      }
+// let config = {
+//     apiKey: "AIzaSyAjBd4hamvW4AsYwymdJOE5Tiw2BHlLZLQ",
+//     authDomain: "collection-69f45.firebaseapp.com",
+//     databaseURL: "https://collection-69f45.firebaseio.com",
+//     projectId: "collection-69f45",
+//     storageBucket: "collection-69f45.appspot.com",
+//     messagingSenderId: "775971698132"
+// };
+let config = {
+    apiKey: "AIzaSyCIINtFQvyR8V2uXTsw8Zd4wa4Oa0p5fgQ",
+    authDomain: "final-project-53d92.firebaseapp.com",
+    databaseURL: "https://final-project-53d92.firebaseio.com",
+    projectId: "final-project-53d92",
+    storageBucket: "final-project-53d92.appspot.com",
+    messagingSenderId: "445822412503"
+};
 
-      class App extends Component {
-        render() {
-          return (
-            <Router>
+firebase.initializeApp(config);
+
+class Authen extends Component {
+    state = {
+        email: '',
+        password: '',
+        err: '',
+        isLoggedIn: false,
+    };
+
+    login = () => {
+        const email = this.state.email;
+        const password = this.state.password;
+        const auth = firebase.auth();
+        const promise = auth.signInWithEmailAndPassword(email, password);
+        promise.then(user => {
+            let message = 'Welcome, you are now logged in!';
+            this.setState({ err: message, isLoggedIn: true });
+            firebase.auth().onAuthStateChanged(user => {
+                if (user) {
+                    window.location = 'search'; //After successful login, user will be redirected to search
+                }
+            });
+
+        });
+
+
+        promise.catch(e => {
+            let err = e.message;
+            this.setState({ err });
+        })
+    };
+
+    signup = () => {
+        const email = this.state.email;
+        const password = this.state.password;
+        const auth = firebase.auth();
+        const promise = auth.createUserWithEmailAndPassword(email, password);
+
+        promise
+            .then(user => {
+                let err = `Welcome ${user.email}`;
+                firebase.database().ref(`users/${user.uid}`).set({
+                    email: user.email,
+                });
+                this.setState({ err });
+            })
+            .catch(e => {
+                let err = e.message;
+                this.setState({ err });
+            });
+    };
+
+    logout = () => {
+        let promise = firebase.auth().signOut();
+        promise.then(() => {
+            let message = 'You are now logged out!';
+            this.setState({ err: message, isLoggedIn: false });
+        }).catch(err => {
+            this.setState({ err: err.message });
+        });
+    };
+
+
+    render() {
+        return (
             <div>
-              {/* <Navbar /> */}
-              <Route exact path="/" component={Welcome} />
-              <Route path="/search" component={SearchPage} />
-              <Route path="/Admin" component={Admin} />
-      
+                <input
+                    id="email"
+                    onChange={(e) => { this.setState({ email: e.target.value }) }}
+                    type="email"
+                    placeholder="Enter your email"
+                /> <br />
+                <input
+                    id="pass"
+                    onChange={(e) => { this.setState({ password: e.target.value }) }}
+                    type="password"
+                    placeholder="Enter your password"
+                /> <br />
+                <p>{this.state.err}</p>
+                <button onClick={this.login}>Login</button>
+                <button
+                    id="logout"
+                    onClick={this.logout}
+                    className={this.state.isLoggedIn ? '' : 'hide'}>Logout</button>
+                <br />
+                <img alt="3" src="/assets/3.jpg" responsive="true" />
             </div>
-             </Router>
-             
-          );
-          
-        }
-      }
-      
-      
-      export default Authen;
-      
+        );
+    }
+}
+
+
+
+export default Authen;
--- src/App.js
+++ src/App.js
@@ -1,92 +1,24 @@
 
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-// import FlatButton from 'material-ui/FlatButton';
-// import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-// import Button from 'material-ui/Button';
-// import { FlatButton } from 'material-ui';
-
-
-import {BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import SearchPage from './components/SearchPage';
 import Welcome from './components/WelcomePage';
-// import Login from './components/Login';
-// import {browserHistory,IndexRoute} from 'react-router';
-// import injectTapEventPlugin from 'react-tap-event-plugin';
-// import getMuiTheme from 'material-ui/styles/getMuiTheme';
-// import Signup from './components/Signup';
-// import Logout from './components/Logout';
-// import { Link, IndexLink } from 'react-router';
-// import AppBar from 'material-ui/AppBar';
-// import { FlatButton } from 'material-ui';
 import Admin from './Admin';
 
 
-
-
-
-// injectTapEventPlugin();
-
-// export  class MainComponent extends React.Component {
-//         render() {
-
-//             return (
-//               <MuiThemeProvider muiTheme={getMuiTheme()}>
-//                  <Router history={browserHistory}>
-//                      <Route path="/" component={App}>
-//                        <IndexRoute component={Login}/>
-//                        <Route path={'Signup'} component={Signup}/>
-//                          <Route path={'Login'} component={Login}/>
-//                          <Route path={'Logout/:user'} component={Logout}/>
-//                        </Route>
-//                  </Router>
-//              </MuiThemeProvider>
-//            );
-//             }
-
-
-
-//         }
-
-//         export class Main extends React.Component{
-//           constructor(props){
-//             super(props);
-//           }
-//           render(){
-//             return(
-//               <div className='container-fluid'>
-//                 <AppBar title="React Login App" id="title"
-//                 showMenuIconButton = {false}
-//                   iconElementRight = { <div className="top-bar-right">
-//                   <Link to="/login"><Button label="Login"/></Link>
-        
-//                 </div>}
-        
-//                 />
-//               {this.props.children}
-//              </div>
-//           );
-        
-//           }
-//         }
-        
 
 class App extends Component {
   render() {
     return (
       <Router>
-      <div>
-        {/* <Navbar /> */}
-        <Route exact path="/" component={Welcome} />
-        <Route path="/search" component={SearchPage} />
-        <Route path="/Admin" component={Admin} />
-
-      </div>
-       </Router>
-       
+        <div>
+          <Route exact path="/" component={Welcome} />
+          <Route path="/search" component={SearchPage} />
+          <Route path="/Admin" component={Admin} />
+        </div>
+      </Router>
     );
-    
   }
 }
 
