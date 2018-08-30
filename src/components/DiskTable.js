691d9b19d7be3c8f45c37f287090eec10ef2e217
vikrep <protsenko.viktor@gmail.com>
2018-08-30 01:19:28 +0100

    add API post album

--- src/components/DiskTable.js
+++ src/components/DiskTable.js
@@ -24,7 +24,7 @@ class DiskTable extends Component {
     componentDidMount() {
         if (this.state.isFetched === false) {
             this.setState({ isLoading: true });
-            fetch(`https://fierce-refuge-31884.herokuapp.com/api/disk/${this.state.titleId}`) // fetch from Heroku database
+            fetch(`http://localhost:5000/api/disk/${this.state.titleId}`) // fetch from Heroku database
                 .then((response) => {
                     if (response.status >= 200 && response.status < 300) {
                         return response;
--- src/components/InputData.js
+++ src/components/InputData.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import Dropzone from 'react-dropzone';
 import upload from 'superagent'
-import { Table, Button, TableBody, TableRow, TableCell, TableHeader, Image } from 'semantic-ui-react'
+import { Table, Button, TableBody, TableRow, TableCell, TableHeader, Image, Form } from 'semantic-ui-react'
 import './styles/InputData.css'
 class InputData extends Component {
   constructor(props) {
@@ -9,9 +9,22 @@ class InputData extends Component {
     this.state = {
       files: [],
       disabled: true,
-      imageUrl: ''
+      imageUrl: '',
+      artist: '',
+      title: '',
+      year: 0,
+      rating: 1,
+      id: '',
+      country: '',
+      label: '',
+      format: '',
+      genre: '',
+      style: '',
+      credits: ''
     };
   }
+
+  handleChange = (e, { name, value }) => this.setState({ [name]: value })
 
   onDrop = (files) => {
     this.setState({ files });
@@ -19,7 +32,7 @@ class InputData extends Component {
   // https://fierce-refuge-31884.herokuapp.com/upload
   // http://localhost:5000/upload
 
-  handleOnSubmit = (event) => {
+  handleOnSubmitImage = (event) => {
     upload.post('https://fierce-refuge-31884.herokuapp.com/upload')
       .attach('imageFile', this.state.files[0])
       .end((err, res) => {
@@ -28,6 +41,22 @@ class InputData extends Component {
       })
     this.setState({ imageUrl: `https://s3.eu-west-2.amazonaws.com/diskcovers/${this.state.files[0].name}` })
   }
+  handleOnSubmitForm = (event) => {
+    upload.post('http://localhost:5000/upload/form')
+      .type('form')
+      .send({
+        cover: this.state.imageUrl, artist: this.state.artist,
+        title: this.state.title, year: this.state.year, rating: this.state.rating,
+        id: this.state.id, country: this.state.country, label: this.state.label,
+        format: this.state.format, genre: this.state.genre, style: this.state.style,
+        credits: this.state.credits
+      })
+      .end((err, res) => {
+        if (err) console.log(err);
+        alert('Form uploaded!');
+      })
+  }
+
   onMouseOver = (event) => {
     console.log(this.state.imageUrl)
     this.setState({ disabled: !this.state.disabled })
@@ -41,12 +70,12 @@ class InputData extends Component {
             <TableRow>
               <TableCell>
                 <Button color="blue">Save Draft</Button>
-                <Button color="green" onClick={this.handleOnSubmit}>Submit</Button>
+                <Button color="green" onClick={this.handleOnSubmitImage}>Submit Image</Button>
               </TableCell>
             </TableRow>
           </TableHeader>
           <TableBody>
-            <TableRow>
+            <TableRow key="dropzone">
               <TableCell>
                 <div className="dropzone" onMouseOver={this.onMouseOver}>
                   <Dropzone accept="image/jpeg" onDrop={this.onDrop} multiple={false}
@@ -68,6 +97,55 @@ class InputData extends Component {
                     }
                   </ul>
                 </aside>
+              </TableCell>
+            </TableRow>
+            <TableRow key="artist-title">
+              <TableCell>
+                <Form>
+                  <Form.Group unstackable widths='equal'>
+                    <Form.Input label='Artist' placeholder='Artist' name='artist' onChange={this.handleChange} />
+                    <Form.Input label='Title' placeholder='Title' name='title' onChange={this.handleChange} />
+                  </Form.Group>
+                </Form>
+              </TableCell>
+            </TableRow>
+            <TableRow key="year-rating-id">
+              <TableCell>
+                <Form>
+                  <Form.Group unstackable widths='equal'>
+                    <Form.Input type='number' label='Year' placeholder='Year' name='year' onChange={this.handleChange} />
+                    <Form.Input type='number' max='5' min='1' label='Rating' placeholder='Rating' name='rating' onChange={this.handleChange} />
+                    <Form.Input label='Catalog#' placeholder='Catalog#' name='id' onChange={this.handleChange} />
+                  </Form.Group>
+                </Form>
+              </TableCell>
+            </TableRow>
+            <TableRow key="country-label-format">
+              <TableCell>
+                <Form>
+                  <Form.Group unstackable widths='equal'>
+                    <Form.Input label='Country' placeholder='Country' name='country' onChange={this.handleChange} />
+                    <Form.Input label='Label' placeholder='Label' name='label' onChange={this.handleChange} />
+                    <Form.Input label='Format' placeholder='Format' name='format' onChange={this.handleChange} />
+                  </Form.Group>
+                </Form>
+              </TableCell>
+            </TableRow>
+            <TableRow key="genre-style-credits">
+              <TableCell>
+                <Form>
+                  <Form.Group unstackable widths='3'>
+                    <Form.Input label='Genre' placeholder='Genre' width='3' name='genre' onChange={this.handleChange} />
+                    <Form.Input label='Style' placeholder='Style' width='3' name='style' onChange={this.handleChange} />
+                    <Form.Input label='Credits' placeholder='Credits' width='10' name='credits' onChange={this.handleChange} />
+                  </Form.Group>
+                </Form>
+              </TableCell>
+            </TableRow>
+            <TableRow className="table-header-align-right">
+              <TableCell>
+                <Button color="blue">Save Draft</Button>
+                <Button color="green" onClick={this.handleOnSubmitForm}>Submit Form</Button>
               </TableCell>
             </TableRow>
           </TableBody>
